339603e8641a12b30d4eea6b133b4682af275921
Ethan Hall <ethanwhall@icloud.com>
2023-06-16 20:23:15 -0400

    work on layout

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-let gridContainer = document.querySelector('.gridContainer');
-for (i = 0; i < 256; i++) {
-    let box = document.createElement('div');
-    box.classList.add('box');
-    gridContainer.appendChild(box);
-    
-    box.addEventListener('mouseover', () => {
-        box.style.background = 'blue';
-    })
-}
+
 
 
 // box.addEventListener('hover', () => {
 //     box.style.background = 'blue';
 // })
+
+
+let gridContainer = document.querySelector('.gridContainer');
+
+function createGrid (squares) {
+    let totalSquares = squares * squares;
+    for (let i = 0; i < totalSquares; i++) {
+        gridContainer.removeChild(square);
+        let square = document.createElement('div');
+        gridContainer.appendChild(square);
+        square.classList.add('box');
+
+
+        square.style.width = `${520 / squares}px`;
+        square.style.height = `${520 / squares}px`;
+
+        square.addEventListener('mouseover', () => {
+            square.style.background = 'blue';
+        })
+    }
+}
+
+
+createGrid(16);
+
+
+let bttnChangeSize = document.querySelector('.bttnChangeGrid');
+bttnChangeSize.addEventListener('click', () => {
+    let size = prompt("Define a number for one side of the grid:")
+    gridContainer.removeChild(square);
+    createGrid(size);
+})
