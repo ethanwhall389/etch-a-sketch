d08ff0c2f5ca80c372e31d70fa44b7e3c9d22ff5
Ethan Hall <ethanwhall@icloud.com>
2023-06-16 21:26:05 -0400

    Complete js function, begin ui

--- script.js
+++ script.js
@@ -7,11 +7,12 @@
 
 
 let gridContainer = document.querySelector('.gridContainer');
+let color = 'green';
 
 function createGrid (squares) {
     let totalSquares = squares * squares;
     for (let i = 0; i < totalSquares; i++) {
-        gridContainer.removeChild(square);
+        // gridContainer.removeChild(square);
         let square = document.createElement('div');
         gridContainer.appendChild(square);
         square.classList.add('box');
@@ -21,7 +22,7 @@ function createGrid (squares) {
         square.style.height = `${520 / squares}px`;
 
         square.addEventListener('mouseover', () => {
-            square.style.background = 'blue';
+            square.style.background = color;
         })
     }
 }
@@ -31,8 +32,36 @@ createGrid(16);
 
 
 let bttnChangeSize = document.querySelector('.bttnChangeGrid');
-bttnChangeSize.addEventListener('click', () => {
+bttnChangeSize.addEventListener('click', newGrid);
+
+function newGrid () {
     let size = prompt("Define a number for one side of the grid:")
-    gridContainer.removeChild(square);
+    if (size > 100 || size < 1) {
+        alert('Size must be between 1 and 100. Please enter another number.')
+        return;
+    }
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
     createGrid(size);
-})
+}
+
+
+
+    let optionBlue = document.querySelector('.optionBlue');
+    let optionGreen = document.querySelector('.optionGreen');
+    let optionRed = document.querySelector('.optionRed');
+    optionBlue.addEventListener('click', () => {
+        color = 'blue'
+        console.log ('click');
+    });
+
+    optionGreen.addEventListener('click', () => {
+        color = 'green'
+        console.log ('click');
+    });
+
+    optionRed.addEventListener('click', () => {
+        color = 'red'
+        console.log ('click');
+    });
